c3709001fbfd6adc4d032a6cfbd2fc84ff96e2e6
phlpsn <philipsenwout@outlook.com>
2021-08-20 15:05:16 +0200

    03: setup database values for models and adjust the User model to our needs

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
+        'username',
+        'first_name',
+        'last_name',
         'email',
         'password',
     ];
