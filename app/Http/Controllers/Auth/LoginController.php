f462059a6b45001e7aa18fa2a78e409aa8a19ef7
phlpsn <philipsenwout@outlook.com>
2021-08-23 12:32:44 +0200

    05: fix logout in dashboard Ã& set max attempts for failed login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -28,6 +28,9 @@ class LoginController extends Controller
      */
     protected $redirectTo = RouteServiceProvider::HOME;
 
+    protected $maxAttempts = 3; // default is five
+    protected $decayMinutes = 5; // default is one
+
     /**
      * Create a new controller instance.
      *
