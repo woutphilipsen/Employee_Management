466d71f44ac5ae001e864a9d370bbd4bd69dc651
phlpsn <philipsenwout@outlook.com>
2021-08-23 15:04:05 +0200

    08: update password

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -23,7 +23,23 @@
                 @endif
             </div>
             <div class="card-header">
-                <a href="{{ route('users.create') }}" class="float-right">Create User</a>
+                <div class="row">
+                    <div class="col">
+                        <form method="GET" action="{{ route('users.index') }}">
+                            <div class="form-row align-items-center">
+                                <div class="col">
+                                    <input type="search" name="search" class="form-control mb-2" id="inlineFormInput" placeholder="Jane Doe">
+                                </div>
+                                <div class="col">
+                                    <button type="submit" class="btn btn-primary mb-2">Search</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div>
+                        <a href="{{ route('users.create') }}" class="btn btn-primary mb-2">Create</a>
+                    </div>
+                </div>
             </div>
             <div class="card-body">
                 <table class="table">
